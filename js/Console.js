98ad9bfe8e9d560cf65f944740b0ead4020d6ffe
DuCun <58730447+DuCun@users.noreply.github.com>
2020-05-11 12:00:06 +0000

    Update Console.js

--- js/Console.js
+++ js/Console.js
@@ -1,1 +1,6 @@
-
+//console
+console.clear();
+console.log("%c 希望DDOS的大佬手下留情 %c","background:#24272A; color:#ffffff","","https://blog.cno1.top/");
+console.log("%c 希望CC的大佬手下留情 %c","background:#24272A; color:#ffffff","","https://blog.cno1.top/");
+console.log("%c 希望扒站的大佬手下留情 %c","background:#24272A; color:#ffffff","","https://blog.cno1.top/");
+console.log('%c 页面加载完毕消耗了'+Math.round(performance.now()*100)/100+'ms','background:#fff;color:#333;text-shadow:0 0 2px #eee,0 0 3px #eee,0 0 3px #eee,0 0 2px #eee,0 0 3px #eee;');
